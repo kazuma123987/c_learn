57da4a9496db17b6024a9a44e2c8c7ff07d43af5
kazuma123987 <3418162880@qq.com>
2025-06-07 17:27:07 +0800

    删除两个代码仓模块

--- src/zlgcan_demo/zlgcan_demo.h
+++ src/zlgcan_demo/zlgcan_demo.h
@@ -1,6 +1,8 @@
 #ifndef __ZLGCAN_DEMO__H
 #define __ZLGCAN_DEMO__H
 // header for gui
+#define DEFAULT_WINDOW_WIDTH 1920
+#define DEFAULT_WINDOW_HEIGHT 1080
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_vulkan.h"
@@ -9,7 +11,11 @@
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
+// Volk headers
+#ifdef IMGUI_IMPL_VULKAN_USE_VOLK
+#define VOLK_IMPLEMENTATION
+#include <volk.h>
+#endif
 // std headers
 #include <iostream>
 #include <vector>
